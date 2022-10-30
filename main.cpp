1a53abc433dbd3b8ccb568245cd79f9a1b783a0a
Markkk <103251001+markhoracebalais@users.noreply.github.com>
2022-10-30 10:15:05 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// File Name: main.cpp
+// Author: Mark Balais
+// Activity 5 CPE 411 Embedded System
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include "includes/crc.h"
+using namespace std;
+
+int main() 
+{   
+    //Driver Code for Cyclic Redundancy Check
+    getdata();
+    sender_side();
+    receiver_side();
+    return 0;
+}
